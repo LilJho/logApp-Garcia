b89c368af32dc14fbab832223361b5448c655296
Jhonnel <jhonnelg25@gmail.com>
2021-10-29 07:25:15 +0800

    changes made

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/logApp-Garcia/');
+	define('ROOT_URL', 'https://mysterious-tor-26598.herokuapp.com/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447458');
 	define('DB_PASS', 'FiiQndG7VU');
